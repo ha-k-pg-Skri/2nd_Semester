d383bec524958c37c75f4518d5b27e44d1b694f0
MSI\kouta <kouta9195@gmail.com>
2020-10-06 14:27:11 +0900

    リファクタリングその１ クラス名変更 クラスの定義を別のcpp,hに移動 よく使うヘッダーファイルのインクルードを、Commonに集約

--- 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
+++ 01_1006/ClassPractice/ClassPractice/ClassPractice.cpp
@@ -1,70 +1,24 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
-
-
-class Fighter
-
-{
-
-public:
-
-	//コンストラクタ
-	Fighter();
-	
-	//デストラクタ
-	~Fighter();
-
-	int m_hp;
-	int m_str;
-	int m_mag;
-	int m_tec;
-	int m_spd;
-	int m_def;
-	int m_luc;
-	int m_mde;
-
-	
+﻿#include"Comon.h"
+#include"Unit.h"
 
 
 
-};
-
-Fighter::Fighter()
-{
-	 m_hp =0;
-	 m_str =0;
-	 m_mag=0;
-	 m_tec=0;
-	 m_spd=0;
-	 m_def=0;
-	 m_luc=0;
-	 m_mde=0;
-
-}
-Fighter::~Fighter()
-{
-
-
-
-
-
-
-
-}
 
 
 int main()
 {
-	Fighter beast;
+	Unit beast;
 
-	printf("hp =%d\n",beast.m_hp);
-	printf("str =%d\n",beast.m_str);
-	printf("mag =%d\n",beast.m_mag);
-	printf("tec =%d\n",beast.m_tec);
-	printf("spd =%d\n",beast.m_spd);
-	printf("def =%d\n",beast.m_def);
-	printf("luc =%d\n",beast.m_luc);
-	printf("mde =%d\n",beast.m_mde);
+	printf("m_hp =%d\n", beast.m_hp);
+	printf("str =%d\n", beast.m_str);
+	printf("mag =%d\n", beast.m_mag);
+	printf("tec =%d\n", beast.m_tec);
+	printf("spd =%d\n", beast.m_spd);
+	printf("def =%d\n", beast.m_def);
+	printf("luc =%d\n", beast.m_luc);
+	printf("mde =%d\n", beast.m_mde);
+
+
 
 	system("pause");
 	return 0;
