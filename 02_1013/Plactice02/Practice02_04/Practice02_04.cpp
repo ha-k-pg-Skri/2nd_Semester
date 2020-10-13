515a3df3a0a31b0e53bdb9c99b0d923d03fe1d3d
MSI\kouta <kouta9195@gmail.com>
2020-10-13 15:43:28 +0900

    継承・汎化

---
+++ 02_1013/Plactice02/Practice02_04/Practice02_04.cpp
@@ -0,0 +1,33 @@
+﻿#include<stdlib.h>
+#include<stdio.h>
+#include"Player.h"
+#include"Enemy.h"
+
+int main()
+{
+
+	Player player;
+
+	Player.SetMoveSpeed();
+	Enemy enemy;
+	enemy.SetHp(100);
+
+	printf("----------------\n");
+	printf("PRACTICE02_04\n");
+	printf("----------------\n");
+
+	printf("Player Status\n");
+	printf("hp =%d\n", player.GetHp());
+	printf("posX =%0.2f\n", player.GetPosX());
+	printf("posY =%0.2f\n", player.GetPosY());
+	printf("MoveSpeed =%0.2f\n", player.GetMoveSpeed());
+
+	printf("\n");
+
+
+
+
+	system("pause");
+	return 0;
+}
+
