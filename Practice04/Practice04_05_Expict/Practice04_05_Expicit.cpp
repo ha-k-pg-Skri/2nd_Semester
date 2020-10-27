c3025be5b75e95a3f4489809f45040c9d3740acb
MSI\kouta <kouta9195@gmail.com>
2020-10-27 15:46:48 +0900

    第4回実習　その５explicit 書き方使い方 変換コンストラクタについて 暗黙の型変換について

---
+++ Practice04/Practice04_05_Expict/Practice04_05_Expicit.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+
+class A
+{
+public:
+    // explicit 演算子の書き方
+   explicit A(int val)
+    {
+        std::cout << val << std::endl;
+    }
+
+};
+
+class B
+{
+
+public:
+   explicit B(const A& ref)
+    {
+       
+    }
+};
+int main()
+{
+    //引数を１つだけとるコンストラクタは、
+    //暗黙の型変換が行われる可能性がある
+   explicit B(1000);  //BのクラスのコンストラクタはAのクラスの変数をうけとるのでは？
+              //暗黙的にB（A（1000））と解釈しちゃってる
+
+    A test = 100; //Aのクラス変数にデータ型を代入？
+          //暗黙的にA　test =A(100)と解釈しちゃっている
+          //コピーコンストラクタが呼び出されている
+
+    A(35);   //Aのクラスのコンストラクタ（問題的）
+             //問題なし
+  
+}
