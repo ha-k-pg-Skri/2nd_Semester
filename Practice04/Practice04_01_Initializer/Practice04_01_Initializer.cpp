e778fd68cfa49d7ce576b4d283ae21d13530b645
MSI\kouta <kouta9195@gmail.com>
2020-10-27 10:39:24 +0900

    第四回のプロジェクトの作成 メンバイニシャライザの実習

---
+++ Practice04/Practice04_01_Initializer/Practice04_01_Initializer.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+
+class CharaBase
+{
+public:
+    CharaBase();
+    ~CharaBase();
+
+    void PrintParam();
+
+private:
+       
+    int Hp;
+    int Mp;
+
+    const int HP_MAX;
+
+};
+
+//コンストラクタで、メンバイニシャライザによる初期化のやり方
+//　クラス名：：コンストラクタ（引数）　:　メンバ変数（初期値）、メンバ変数（初期値）
+
+CharaBase::CharaBase()
+    : Hp(100)
+    , Mp(10)
+    ,HP_MAX(999)  //constメンバ変数もここでなら初期化できる
+   
+{
+  
+}
+CharaBase::~CharaBase()
+{
+   // HP_MAX = 999;  //コンストラクタ内ではconst型のメンバ変数を初期化できない
+}
+
+void  CharaBase::PrintParam()
+{
+    printf("Hp =%d\n", Hp);
+    printf("Mp =%d\n", Mp);
+    printf("\n");
+    printf("HP_MAXp =%d\n", HP_MAX);
+}
+
+
+int main()
+{
+    CharaBase ch;
+    ch.PrintParam();
+}
