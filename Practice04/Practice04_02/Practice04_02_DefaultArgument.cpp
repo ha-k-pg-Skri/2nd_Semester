b2dcf45d06cc15c372c75251353f9250600d179b
MSI\kouta <kouta9195@gmail.com>
2020-10-27 12:06:36 +0900

    第4回実習その２ デフォルト引数の書き方、使い方、 どのようなデフォルト引数がよいか 末尾につける

---
+++ Practice04/Practice04_02/Practice04_02_DefaultArgument.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+class Calculator
+{
+
+public:
+    int add(int a, int b);
+    int Mul(int a, int b =2);
+
+
+};
+
+int Calculator::add(int a, int b)
+{
+
+    return (a + b);
+}
+
+int Calculator::Mul(int a, int b)
+{
+
+    return (a * b);
+}
+
+
+int main()
+{
+    Calculator calc;
+    std::cout << calc.add(10, 20) << std::endl;
+
+}
+
