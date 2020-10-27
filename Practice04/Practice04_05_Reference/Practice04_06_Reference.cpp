201cfd37bfe7a958d74e259e270d891d92f79ca7
MSI\kouta <kouta9195@gmail.com>
2020-10-27 16:19:03 +0900

    第4回実習その６　 参照渡し　使い方書き方
    変数に別の名をつけてアドレスを共有する

---
+++ Practice04/Practice04_05_Reference/Practice04_06_Reference.cpp
@@ -0,0 +1,43 @@
+﻿// Practice04_05_Reference.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+void Init(int& rhp, int& rmp)
+{
+    rhp = 50;
+    rmp = 10;
+}
+
+int main()
+{
+    int val = 0;
+    int ref = val;
+
+    printf("valのアドレス %x\n", &val);
+    printf("refのアドレス %x\n", &ref);
+
+    printf("valに100を代入\n");
+    val = 100;
+    printf("val= %d\n", val);
+    printf("ref= %d\n", ref);
+
+    printf("\n", ref);
+
+    printf("refに10を代入\n");
+        ref = 10;
+      printf("val =%d\n", val);
+      printf("ref =%d\n", ref);
+
+      //参照渡し
+      //引数に参照を持つ関数へ変数を渡すこと
+
+      int hp = 0;
+      int mp = 0;
+
+      Init(hp, mp);
+      printf("HP =%d\n", hp);
+      printf("MP =%d\n", mp);
+  
+      return 0;
+}
+
