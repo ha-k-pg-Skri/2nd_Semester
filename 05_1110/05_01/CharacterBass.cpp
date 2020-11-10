5daf3c18d67c2507a489f85a14751baadef1e21f
MSI\kouta <kouta9195@gmail.com>
2020-11-10 10:38:47 +0900

    第5回実習その１
    コンストメンバ関数を組みあわせた、クラスの参照渡し

---
+++ 05_1110/05_01/CharacterBass.cpp
@@ -0,0 +1,16 @@
+#include "../05_01/Practice05_01_ClassRefarence/CharacterBass.h"
+
+
+CharacterBass::CharacterBass()
+	:Hp(10)
+	, Mp(8)
+	,Str(6)
+	,Vit(5)
+	,Int(12)
+{}
+
+
+CharacterBass::~CharacterBass()
+{
+
+}
---
+++ 05_1110/05_01/Practice05_01_ClassRefarence/Practice05_01_ClassRefarence.cpp
@@ -0,0 +1,25 @@
+﻿// Practice05_01_ClassRefarence.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+#include"CharacterBass.h"
+#include <iostream>
+
+void PrintParam(const CharacterBass& character);
+//const参照引数をもつパラメーター
+int main()
+{
+	CharacterBass testChar;
+	PrintParam(testChar);
+
+	return 0;
+}
+
+void PrintParam(const CharacterBass& character)
+{
+	printf("Character Parameter List\n");
+	printf("------------------------\n");
+	printf("Hp =%3d\n",character.GetHp());
+	printf("Mp =%3d\n",character.GetMp());
+	printf("Str =%3d\n",character.GetStr());
+	printf("Vit =%3d\n",character.GetVit());
+	printf("Int =%3d\n",character.GetInt());
+}
