541eebda5b9764849aa0a0f3e1dd83732d35c0e5
MSI\kouta <kouta9195@gmail.com>
2020-11-10 11:15:16 +0900

    第5回実習その２-1
    通常のシンプルなNumクラスを作成

---
+++ 05_1110/05_01/StaticClassMember/Num.h
@@ -0,0 +1,20 @@
+#ifndef NUM_H
+#define NUM_H
+
+
+class Num
+{
+public:
+	Num(int nnum);
+	void Print();
+	
+
+private:
+	int m_Num;
+
+};
+
+
+
+
+#endif
---
+++ 05_1110/05_01/StaticClassMember/StaticClassMember.cpp
@@ -0,0 +1,12 @@
+﻿#include"Num.h"
+#include <iostream>
+
+int main()
+{
+	Num a(100), b(200);
+	a.Print();
+	b.Print();
+
+	return 0;
+}
+
